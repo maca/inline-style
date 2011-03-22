ad06e1d1d986c96ff991265e3fed8d6a7fbc3163
Macario Ortega <macarui@gmail.com>
2011-03-22 04:54:41 -0600

    tiny

--- lib/inline-style.rb
+++ lib/inline-style.rb
@@ -14,7 +14,7 @@ class InlineStyle
   #
   # @option opts [String] :stylesheets_path (env['DOCUMENT_ROOT']) 
   #       Stylesheets root path or app's public directory where the stylesheets are to be found
-  # @option opts [Boolean] :pseudo (false) 
+  # @option opts [boolean] :pseudo (false) 
   #       If set to true will inline style for pseudo classes according to the W3C specification:
   #       http://www.w3.org/TR/css-style-attr.
   #       Should probably be left as false because browsers don't seem to comply with the specification for pseudo class style in the style attribute.
